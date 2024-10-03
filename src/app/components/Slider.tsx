e3b4c62d3e9e7c05d1ef753cd6fe652c8dbe8ede
baalais <tomassrudzitis2014@gmail.com>
2024-10-03 23:28:15 +0300

    Categories nakosais (1:14:52)
    Categories nakosais (1:14:52)

---
+++ src/app/components/Slider.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import { useState,useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+
+const slides = [
+  {
+    id: 1,
+    title: "Pirmais autokluba pasakums",
+    description: "Skuning baby",
+    img: "https://images.pexels.com/photos/26242007/pexels-photo-26242007/free-photo-of-portrait-of-a-woman-standing-in-a-city.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50"
+  },
+  {
+    id: 2,
+    title: "Pirmais Open car meet",
+    description: "Open car meet baby Valmiera",
+    img: "https://images.pexels.com/photos/10720627/pexels-photo-10720627.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+    url: "/",
+    bg: "bg-gradient-to-r from-pink-50 to-blue-50"
+  },
+  {
+    id: 3,
+    title: "Autokluba merch",
+    description: "Pirmais autokluba merch Midnight Runner hoodie",
+    img: "https://images.pexels.com/photos/4569661/pexels-photo-4569661.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+    url: "/",
+    bg: "bg-gradient-to-r from-blue-50 to-yellow-50"
+  }
+]
+
+const Slider = () => {
+
+  const [current, setCurrent] = useState(0)
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setCurrent((prev) => (prev === slides.length -1 ? 0 : prev + 1))
+    },3000)
+
+    return () => clearInterval(interval);
+  },[])
+
+  return (
+    <div className='h-[calc(100vh-80px)] overflow-hidden'>
+      <div className='w-max h-full flex transition-all ease-in-out duration-1000' style={{transform:`translateX(-${current * 100}vw)`}}>
+        {slides.map(slide=>(
+          <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
+            {/* Text container */}
+            <div className='h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center'>
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
+              <Link href={slide.url}><button className="rounded-md bg-black text-white py-3 px-4">SHOP NOW</button></Link>
+            </div>
+            {/* Image container */}
+            <div className='h-1/2 xl:w-1/2 xl:h-full relative'>
+              <Image src={slide.img} alt="" fill sizes="100%" className='object-cover'/>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+        {slides.map((slide,index)=>(
+          <div className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-content ${current === index ? "scale-150" : "" }`} key={slide.id}
+          onClick={()=>setCurrent(index)}
+          >
+            { current === index && (<div className='w-[6px] h-[6px] bg-gray-600 rounded-full'></div>)}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider
